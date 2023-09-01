74262947c9b5c9132d2bff7d2aaa7b56f9918615
slecler96 <simon.lecler96@gmail.com>
2023-09-01 16:27:04 +0200

    Write the final comments in the js file

--- calculator.js
+++ calculator.js
@@ -20,66 +20,46 @@ let currentOperator = EMPTY;
 let currentResult = EMPTY;
 
 /*
-Deal with keyboard inputs
-*/
-window.addEventListener('keydown', dealWithKeyboardInput)
-
-function dealWithKeyboardInput(e) {
-    if (0 <= parseInt(e.key) && parseInt(e.key) <= 9) {
-        appendDigit(e.key);
-    }
-    else if ([PLUS,MINUS,MULTIPLY,DIVIDE].includes(e.key)) {
-        appendOperator(convertOperator(e.key));
-    }
-    else if (e.key === "Backspace") {
-        backspace() 
-    }
-    else if (e.key === "Escape") {
-        clear()
-    }
-    else if (["=", "Enter"].includes(e.key)) {
-        performOperation()
-    }
-    else if (e.key === POINT) {
-        appendPoint()
-    }
-}
-
-function convertOperator(operator) {
-    if (operator === DIVIDE) {
-        return "÷";
-    }
-    else if (operator === MULTIPLY) {
-        return "x"
-    }
-    else {
-        return operator
-    }
-}
-
-/*
-Activate digit buttons
+Activate digit and operators buttons
 */
 digitButtons.forEach(button => {button.addEventListener('click', () => appendDigit(button.textContent))});
 operatorButtons.forEach(button => {button.addEventListener('click', () => appendOperator(button.textContent))});
-
+equalButton.addEventListener('click', performOperation);
 
 /*
 Clear the screen
 */
 clearButton.addEventListener('click', clear);
 
+/*
+Backspace button
+*/
+deleteButton.addEventListener('click', backspace);
+
+/*
+Deal with keyboard inputs
+*/
+window.addEventListener('keydown', dealWithKeyboardInput)
+
+/*
+Point button to write float numbers
+*/
+floatButton.addEventListener('click', appendPoint);
+
+
+/*
+Clear the screen
+*/
 function clear() {
     screenCurrent.textContent = EMPTY;
     screenResult.textContent = EMPTY;
     currentFirstNumber = currentSecondNumber = currentOperator = currentResult = EMPTY
 }
 
-/*
-Float button to write float numbers
-*/
-floatButton.addEventListener('click', appendPoint);
 
+/*
+Append a point to the current number to deal with floating point numbers
+*/
 function appendPoint() {
     if (currentOperator === EMPTY) {
         if (!currentFirstNumber.includes(POINT)) {
@@ -96,12 +76,10 @@ function appendPoint() {
 }
 
 
-
 /*
-Delete the last displayed character on screen
+When the user presses the "delete" button or the backspace key, delete the last character displayed on screen 
+and update the corresponding number or operator variable
 */
-deleteButton.addEventListener('click', backspace);
-
 function backspace() {
     
     if (currentSecondNumber === EMPTY) {
@@ -126,8 +104,10 @@ function backspace() {
 }
 
 
-equalButton.addEventListener('click', performOperation);
 
+/*
+When the user presses = : performs the given operation and displays the result on screen
+*/
 function performOperation() {
     let result;
     if (currentSecondNumber === EMPTY) {
@@ -151,6 +131,9 @@ function performOperation() {
 
 }
 
+/*
+Add digit to the corresponding number and update the screen
+*/
 function appendDigit(digit) {
     screenResult.textContent = EMPTY;
     if (currentOperator === EMPTY) {
@@ -164,6 +147,10 @@ function appendDigit(digit) {
     }   
 }
 
+/*
+Record operator selected by the user, perform previous operation if 2 numbers have already been given (the result 
+    becomes the new first number in the current operation) and displays the corresponding operator on screen
+*/
 function appendOperator(operator) {
     screenResult.textContent = EMPTY;
     
@@ -197,6 +184,7 @@ function appendOperator(operator) {
 
 }
 
+
 function add(firstNumb, secondNumb) {
     let result = +firstNumb + +secondNumb;
     return +firstNumb + +secondNumb;
@@ -214,6 +202,10 @@ function divide(firstNumb, secondNumb) {
     return +firstNumb / +secondNumb;
 }
 
+/*
+Perform operation defined by operator between firstNumb and secondNumb
+Returns null when trying to divide by 0
+*/
 function operate(operator, firstNumb, secondNumb) {
     switch (operator) {
         case '+':
@@ -229,7 +221,40 @@ function operate(operator, firstNumb, secondNumb) {
             else {
                 return divide(firstNumb, secondNumb);
             }
-        default:
-            return None
+    }
+}
+
+
+
+function dealWithKeyboardInput(e) {
+    if (0 <= parseInt(e.key) && parseInt(e.key) <= 9) {
+        appendDigit(e.key);
+    }
+    else if ([PLUS,MINUS,MULTIPLY,DIVIDE].includes(e.key)) {
+        appendOperator(convertOperator(e.key));
+    }
+    else if (e.key === "Backspace") {
+        backspace() 
+    }
+    else if (e.key === "Escape") {
+        clear()
+    }
+    else if (["=", "Enter"].includes(e.key)) {
+        performOperation()
+    }
+    else if (e.key === POINT) {
+        appendPoint()
+    }
+}
+
+function convertOperator(operator) {
+    if (operator === DIVIDE) {
+        return "÷";
+    }
+    else if (operator === MULTIPLY) {
+        return "x"
+    }
+    else {
+        return operator
     }
 }
